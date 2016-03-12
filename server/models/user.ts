e4f23caeff74f08880031ddfbb671529c64a94f3
Wangzhetuteng <ericdicaprio@outlook.com>
2016-03-11 23:26:11 -0500

    Initial Commit

---
+++ server/models/user.ts
@@ -0,0 +1,27 @@
+import mongoose = require('mongoose');
+
+// DEFINE THE OBJECT SCHEMA
+var userSchema = new mongoose.Schema ({
+   created: {
+       type: Date,
+       default:Date.now
+   } ,
+   user_name: {
+       type: String,
+       default: '',
+       trim: true,
+       required: 'User Name is required'
+   },
+   password: {
+       type: String,
+       default: '',
+       trim: true,
+       required: 'Password is required'
+   }
+}, 
+{
+    collection:"userInfo"
+});
+
+// MAKE THIS PUBLIC SO THE CONTROLLER CAN SEE IT
+export var User = mongoose.model('User', userSchema);
